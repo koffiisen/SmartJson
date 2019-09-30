5069d09c7246aa8da473adb5a467df414f840182
Koffi Joel <koffi_joel.onipoh@isen.yncrea.fr>
2019-09-30 22:35:43 +0200

    update support python 2.7

--- scripts/__smart_json__.py
+++ scripts/__smart_json__.py
@@ -94,9 +94,6 @@ class SmartJson:
 
     class _UnsupportedClass(Exception):
         def __init__(self, message, errors):
-            # Call the base class constructor with the parameters it needs
-            super().__init__(message)
-
             # Now for your custom code...
             self.errors = errors
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     setup(
         name='smartjson',
-        version='0.0.1',
+        version='0.0.2',
         author="J. Koffi ONIPOH",
         author_email="jolli644@gmail.com",
         description="A tool to convert any class to json and convert json to object",
@@ -13,10 +13,10 @@ with open("README.md", "r") as fh:
         url="https://github.com/koffiisen/SmartJson",
         keywords=['Json', 'Class', 'Object to json', 'Class to json', 'json to object', 'json to class'],
         packages=find_packages(),
-        python_requires='>=3',
+        python_requires='>=2.7',
         include_package_data=False,
         classifiers=[
-            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2.7",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
