55a83646bd58830f3f56d55d141dbd865782b854
Koffi Joel <koffi_joel.onipoh@isen.yncrea.fr>
2019-09-30 22:10:04 +0200

    update license

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
         description="A tool to convert any class to json and convert json to object",
         long_description=long_description,
         long_description_content_type='text/markdown',
-        url="https://github.com/koffiisen/SuperJson",
+        url="https://github.com/koffiisen/SmartJson",
         keywords=['Json', 'Class', 'Object to json', 'Class to json', 'json to object', 'json to class'],
         packages=find_packages(),
         python_requires='>=3',
